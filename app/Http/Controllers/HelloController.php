253b59ee8970cef4760f73ee654ed2a536f943a5
weipa7 <hajime0578@gmail.com>
2022-03-23 11:48:16 +0900

    init

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index() {
+        $data = ['one', 'two', 'three', 'four', 'five'];
+        return view('hello.index', ['data'=>$data]);
+    }
+    
+    public function post(Request $request) {
+        return view('hello.index', ['msg' => $request->msg]);
+    }
+}
---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+	@parent
+	インデックスページ
+@endsection
+
+@section('content')
+	<p>ここが本文のコンテンツです。</p>
+	<p>必要なだけ記述できます。</p>
+	
+	@component('components.message')
+		@slot('msg_title')
+		CAUTION!
+		@endslot
+		
+		@slot('msg_content')
+		これはメッセージ表示です。
+		@endslot
+	@endcomponent
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
