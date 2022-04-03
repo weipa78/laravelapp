46e1329a0bf05f3199e4e4dd4b919ce8ac014d3f
weipa7 <hajime0578@gmail.com>
2022-04-03 22:10:38 +0900

    auth

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Person;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
+        $user = Auth::user();
         $sort = $request->sort;
         $items = Person::orderBy($sort, 'asc')
-            ->paginate(5);
+            ->simplePaginate(5);
         $param = [
             'items' => $items,
             'sort' => $sort,
+            'user' => $user
         ];
         return view('hello.index', $param);
     }
@@ -81,4 +84,26 @@ class HelloController extends Controller
         $request->session()->put('msg', $msg);
         return redirect('hello/session');
     }
+    
+    public function getAuth(Request $request) {
+        $param = [
+            'message' => 'ログインしてください。'
+        ];
+        return view('hello.auth', $param);
+    }
+    
+    public function postAuth(Request $request) {
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $msg = 'ログインしました。(' . Auth::user()->name . ')';
+        } else {
+            $msg = 'ログインに失敗しました。';
+        }
+        return view('hello.auth', ['message' => $msg]);
+    }
+    
+    
+    
+    
 }
